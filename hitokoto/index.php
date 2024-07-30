9e2afd719f45306bd6a9bec251c7200bb753321e
Physics_nya <676266673@qq.com>
2024-07-30 20:08:37 +0800

    Site updated: 2024-07-30 20:08:35

---
+++ hitokoto/index.php
@@ -0,0 +1,35 @@
+<?php
+header("Content-type: text/html; charset=utf-8"); //设置编码 utf-8 
+//获取句子文件的绝对路径
+$path = dirname(__FILE__);
+ 
+//接受type参数
+if (isset($_GET['type']) && !empty($_GET['type'])) {
+    $type = $_GET['type'];
+    $file = file($path."/".$type.".txt");
+} else {
+    $file = file($path."/hitokoto.txt");
+}
+ 
+ 
+//随机读取一行
+$arr  = mt_rand( 0, count( $file ) - 1 );
+$content  = trim($file[$arr]);
+ 
+//编码判断，用于输出相应的响应头部编码
+if (isset($_GET['charset']) && !empty($_GET['charset'])) {
+    $charset = $_GET['charset'];
+    if (strcasecmp($charset,"gbk") == 0 ) {
+        $content = mb_convert_encoding($content,'gbk', 'utf-8');
+    }
+} else {
+    $charset = 'utf-8';
+}
+
+//格式化判断，输出js或纯文本
+if ($_GET['encode'] === 'js') {
+    echo "function hitokoto(){document.write('" . $content ."');}";
+} else {
+    echo $content;
+}
+?>
